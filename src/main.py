2c57b7023b2467651e86c130c5623dea131e8ca9
danimp94 <danimp94@users.noreply.github.com>
2021-10-26 13:29:52 +0200

    main update

--- src/main.py
+++ src/main.py
@@ -3,29 +3,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-filename = ("..\data\coconut_1.jpg")
-img = cv.imread(filename2)
-
-cv.imshow('img',img)
-
 # Input Image
-def input_image(filename):
-
-    img = cv.imread(filename)
+filename = ("data\coconut_1.jpg")
+img = cv.imread(filename)
 
 # Enhancing Process
-def preprocessing_image(image):
 
     # Portable Gray Map
-    img_grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+img_grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Enhancing Image:
     # Histogram Equalization 
-    img_hist = cv.equalizeHist(img_grey)
+img_hist = cv.equalizeHist(img_grey)
     # Canny edge detection
+img_edge = cv.Canny(img_hist,200,600) #Default parameters:(100,200)
     # Closing and Opening Morphology
+img_dil = cv.dilate(
+    img_edge,
+    cv.getStructuringElement(cv.MORPH_RECT, (2, 2)),
+    iterations=1
+)
 
-    return img_hist
+img_ero = cv.erode(
+    img_dil,
+    cv.getStructuringElement(cv.MORPH_RECT, (2, 2)),
+    iterations=1
+)
+
 
 ## Separability Filter
 #def separability_filter():
@@ -35,7 +39,12 @@ def preprocessing_image(image):
     
 
 #input_image(filename)
-preprocessing_image(img)
+#preprocessing_image(img,0)
 
-cv.imshow(img_hist)
+cv.imshow('img',img)
+cv.imshow('img_grey',img_grey)
+cv.imshow('img_histogram_equalization',img_hist)
+cv.imshow('img_canny_edge',img_edge)
+cv.imshow('img_dilate',img_dil)
+cv.imshow('img_erode',img_ero)
 cv.waitKey(0)
