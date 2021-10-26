c5c88d665486aea346c1f325bb450bb73d1cb98e
danimp94 <danimp94@users.noreply.github.com>
2021-10-26 10:36:44 +0200

    starting

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+# Input Image
+# Enhancing Process
+# Portable Gray Map
+# Enhancing Image:
+# Histogram Equalization 
+# Canny edge detection
+# Closing and Opening Morphology
