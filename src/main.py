9cadb184af1fca65114664ef1446790e1af83298
Resh404 <Reshad_z@hotmail.com>
2021-10-29 19:32:15 +0200

    Added hough function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,37 @@
+import operator
 import cv2 as cv
 import numpy as np
 import sys
 import time
 import numpy as np
 import  itertools
-from numpy.core.records import array
-np.set_printoptions(threshold=sys.maxsize)
+
+def output_circles(image,x0,y0,r,color,thickness):
+    for t in range(0,360):
+        x=int(x0+r*(np.cos(np.radians(t))))
+        y=int(y0+r*(np.sin(np.radians(t))))
+        for d in range(thickness):
+            image.itemset((y,x,0),color[0])
+            image.itemset((y,x,1),color[1])
+            image.itemset((y,x,2),color[2])
+    cv.imshow("Circled Image",image)
+
+def houghTra(sepX,sepY,sepR):
+    accumulator = {}
+    # Looping over the values of theta, Considering only even values for for minimizing performance time by setting increment as 2
+    for t in range(0,360,2):
+        # Determining all the possible centres x0,y0 using the above formula
+        x0 = int(sepX-(sepR*np.cos(np.radians(t))))
+        y0 = int(sepY-(sepR*np.sin(np.radians(t))))
+        # Checking if the center is within the range of image
+        if x0>0 and x0<width and y0>0 and y0<height:
+            # Voting process...
+            if (x0,y0,sepR) in accumulator:
+                accumulator[(x0,y0,sepR)]=accumulator[(x0,y0,sepR)]+1
+            else:
+                accumulator[(x0,y0,sepR)]=0
+    entire_sorted_accumulator = sorted(accumulator.items(),key=operator.itemgetter(1),reverse=True)
+    return entire_sorted_accumulator[:5]
 
 def pixiExt(img,x0,y0,radius):
     pixsLoca = []
@@ -20,7 +46,7 @@ def pixiExt(img,x0,y0,radius):
     
     dist_from_center = np.sqrt((x0-x)**2 + (y0-y)**2)
     mask0 = dist_from_center <= radius
-    mask1 = dist_from_center <= radius*1.25
+    mask1 = dist_from_center <= radius*1.1
     mask2 = mask1 & ~mask0
     
     height1,width1 = mask0.shape
@@ -62,16 +88,17 @@ if __name__ == "__main__":
     
     height,width = dst.shape
     Rmin = 10
-    Rmax = 30
-    regionRatioContainer = []
+    Rmax = 40
+    highChanObj = []
     
     #print(pixiExt(dst,250,250,50))
     #time.sleep(10)
+    #color: (0,255,0)
     
-    for y, x, r in itertools.product(range(0,height,20), range(0,width,20),range(Rmin,Rmax,2)):
-        if x-Rmax>=0 and x+Rmax<=width and y-Rmax>=0 and y+Rmax<=height:
+    for y, x, r in itertools.product(range(0,height,20), range(0,width,20),range(Rmin,Rmax,4)):
+        if x-Rmin>=0 and x+Rmin<=width and y-Rmin>=0 and y+Rmin<=height:
             pixisR1,intenR1,pixInValR1,uPi,uIn,uInVal= pixiExt(dst,x,y,r)
-            pixisR2,intenR2,pixInValR2,uPi,uIn,uInVal  = pixiExt(dst,x,y,r*1.25)
+            pixisR2,intenR2,pixInValR2,uPi,uIn,uInVal  = pixiExt(dst,x,y,r*1.1)
             regionRatio = paraCal(pixisR1,pixisR2,intenR1,intenR2,pixInValR1,pixInValR2,uPi,uIn,uInVal)
             
             height0,width0 = dst.shape
@@ -79,14 +106,20 @@ if __name__ == "__main__":
             
             dist_from_center = np.sqrt((x - x3)**2 + (y-y3)**2)
             mask0 = dist_from_center <= r
-            mask1 = dist_from_center <= r*1.25
+            mask1 = dist_from_center <= r*1.1
             mask2 = mask1 & ~mask0
             coppyimg = dst.copy()
             coppyimg[mask2] = 255
+            
             cv.imshow("elCircle", coppyimg)
-            cv.waitKey(0)
-            regionRatioContainer.append(regionRatio)
-            print("\r",regionRatio)
+            cv.waitKey(1)
+            
+            if regionRatio != None and regionRatio > 0.75 and regionRatio < 2:
+                highChanObj.append({x,y,r,regionRatio})
+                print("\r",regionRatio)
+                print(houghTra(x,y,r))
             
         sys.stdout.write("\r" + str(int(float(y+1)/float(height)*100))+"%")
         sys.stdout.flush()
+    
+    
